6e7e9502111c0347843095d765be790f51d182f8
Andreas Adam <andreas.adam@composableweb.com>
2021-09-18 10:12:45 +0200

    chore: add tailwind, postcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,12 +23,7 @@ export default function Index() {
 
   return (
     <div style={{ textAlign: "center", padding: 20 }}>
-      <h2>Welcome to Remix!</h2>
-      <p>
-        <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
-        started.
-      </p>
-      <p>Message from the loader: {data.message}</p>
+      <h1 className="logo"><span className="logo__brush">Pixel</span><span className="logo__chrome">mord</span></h1>
     </div>
   );
 }
